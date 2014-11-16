3da8607ca9a884a84d716e5ed3ccbf0b36a997c1
Ana <agiraldow@gmail.com>
2014-11-15 19:04:49 -0500

    Change link colors to red

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+	
   if (msg.command && (msg.command == "revere")) {
     var src = document.getElementsByTagName("title")[0].innerHTML;
     var dst = msg.title;
@@ -7,20 +9,21 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
     $("body").html($("body").html().replace(/North Korea/g,"Democratic People's Republic of Korea"));
 
-		//Enlargen font size of the Dear Leader's name
-		$("body").html($("body").html().replace(/Kim Jong-Un/g,"<span style='font-size:120%'>Kim Jong-un</span>"));
-		$("body").html($("body").html().replace(/Kim Il-sung/g,"<span style='font-size:120%'>Kim Il-sung</span>"));
-		$("body").html($("body").html().replace(/Kim Jong-il/g,"<span style='font-size:120%'>Kim Jong-il</span>"));
+	//Enlarge font size of the Dear Leader's names
+	$("body").html($("body").html().replace(/Kim Jong-Un/g,"<span style='font-size:190%'>Kim Jong-un</span>"));
+	$("body").html($("body").html().replace(/Kim Il-sung/g,"<span style='font-size:190%'>Kim Il-sung</span>"));
+	$("body").html($("body").html().replace(/Kim Jong-il/g,"<span style='font-size:190%'>Kim Jong-il</span>"));
 
-		//Restyle links into colors of the North Korean flag
-		$("a").css("color","#024fa2");
-		$("a").mouseenter(function() {
-			$(this).css("color","#ed1c27");
-		});
-		$("a").mouseleave(function() {
-			$(this).css("color","#024fa2");
-		});
+	// Restyle links into colors of the North Korean flag
+	$("a").attr("style","color: #ed1c27;");
+	$("a").mouseenter(function() {
+		$(this).attr("style","color: #024fa2;");
+	});
+	$("a").mouseleave(function() {
+		$(this).attr("style","color: #ed1c27;");
+	});
     
     sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");
   }
+
 });
